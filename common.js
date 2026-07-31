9f2075d305fc87b9ef37fef1ee5c9722539f1985
location67ha-cyber <location67ha@gmail.com>
2026-07-31 11:53:18 +0200

    Update Supabase key variable and format

--- common.js
+++ common.js
@@ -2,8 +2,8 @@
    CONFIGURATION SUPABASE & UTILITAIRES GLOBAUX (common.js)
    ========================================================================== */
 
-const SUPABASE_URL = "https://xxlehrxxrcuismlcnwhh.supabase.co"; // Remplacer par votre URL Supabase
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4bGVocnh4cmN1aXNtbGNud2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NDk2MjYsImV4cCI6MjEwMDIyNTYyNn0.148ZtlXhbfSlPJmZ6j2IzVDyGL8wXvhAxfoyxXhmCdw"; // Remplacer par votre clé Anon Supabase
+const SUPABASE_URL = 'https://xxlehrxxrcuismlcnwhh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4bGVocnh4cmN1aXNtbGNud2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NDk2MjYsImV4cCI6MjEwMDIyNTYyNn0.148ZtlXhbfSlPJmZ6j2IzVDyGL8wXvhAxfoyxXhmCdw';
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
